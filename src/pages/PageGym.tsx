522a201c361b26661054034e78037aa19984b16c
Jararered <Jararered@icloud.com>
2025-01-06 10:41:54 -0500

    Add PageGym and PageHome components for gym and user greeting functionality

---
+++ src/pages/PageGym.tsx
@@ -0,0 +1,22 @@
+import EquipmentToggles from "../components/v2/EquipmentToggles";
+import ExerciseToggles from "../components/v2/ExerciseToggles";
+
+const PageGym = () => {
+    return (
+        <div className="page-gym">
+
+            <h1>Gym</h1>
+
+            <div className="card">
+                <EquipmentToggles />
+            </div>
+
+            <div className="card">
+                <ExerciseToggles />
+            </div>
+
+        </div>
+    );
+}
+
+export default PageGym;
---
+++ src/pages/PageHome.tsx
@@ -0,0 +1,11 @@
+import UserGreeting from '../components/v2/UserGreeting';
+
+const PageHome = () => {
+    return (
+        <div className="page-home">
+            <UserGreeting />
+        </div>
+    );
+}
+
+export default PageHome;
