ccc9309806702cf83fc19fcd93a6b89f078b6e9b
Jararered <Jararered@icloud.com>
2025-01-07 07:57:48 -0500

    Refactor Workout interfaces to simplify Circuit structure and enhance ExerciseEntry usage; update example workout data

--- src/interfaces/Workout.tsx
+++ src/interfaces/Workout.tsx
@@ -1,41 +1,38 @@
+import { ExerciseNames } from "./Exercises";
+
+// ExerciseEntry is a single exercise with a name and a list of reps
 export interface ExerciseEntry {
     name: string; // Ex: "Barbell Squat"
     reps: number[]; // Ex: [10, 10, 10]
 }
 
-export type ExerciseList = ExerciseEntry[];
+// Circuit is a list of exercises
+export type Circuit = ExerciseEntry[]; // Ex: [ExerciseEntry, ExerciseEntry]
 
-export interface Circuit {
-    name: string; // Ex: Machine that the circuit is on
-    exercises: ExerciseList; // Ex: [{name: "Barbell Squat", reps: [10, 10, 10]}, {name: "Smith Machine Bench Press", reps: [10, 10, 10]}]
-}
-
-export type CircuitList = Circuit[];
-
-export interface DateDetails {
-    start: Date;
-    end: Date;
-}
+export type WorkoutState = "started" | "paused" | "completed";
 
 export interface WorkoutIndexer {
     circuitIndex: number;
     exerciseIndex: number;
 }
 
-export type WorkoutState = "started" | "paused" | "completed";
-
+export interface DateDetails {
+    start: Date;
+    end: Date;
+}
 
 export interface Workout {
-    name: string; // Ex: "Leg Day"
-    circuits: CircuitList;
+    name: string; // Ex: "Leg Day", set by user
+    circuits: Circuit[]; // Ex: [[ExerciseEntry, ExerciseEntry], [ExerciseEntry, ExerciseEntry]]
 
+    // Fields for tracking workout state when a user starts a workout
     state?: WorkoutState;
     indexer?: WorkoutIndexer;
     date?: DateDetails;
 }
 
-export const DefaultWorkout : Workout = {
-    name: "Default Workout",
+export const EmptyWorkout : Workout = {
+    name: "",
     circuits: []
 };
 
@@ -43,20 +40,23 @@ export const DefaultWorkout : Workout = {
 export const LegsExampleWorkout: Workout = {
     name: "Leg Day",
     circuits: [
-        {
-            name: "Smith Machine Circuit",
-            exercises: [
-                { name: "Smith Machine Squat", reps: [10, 10, 10] },
-                { name: "Smith Machine Front Squat", reps: [10, 10, 10] },
-
-            ]
-        },
-        {
-            name: "Leg Extension Circuit",
-            exercises: [
-                { name: "Leg Extension", reps: [10, 10, 10] },
-                { name: "Leg Curl", reps: [10, 10, 10] }
-            ]
-        }
+        [
+            {
+                name: ExerciseNames.BarbellSquat,
+                reps: [10, 10, 10]
+            },
+            {
+                name: ExerciseNames.LegPress,
+                reps: [10, 10, 10]
+            },
+            {
+                name: ExerciseNames.LegCurl,
+                reps: [10, 10, 10]
+            },
+            {
+                name: ExerciseNames.LegExtension,
+                reps: [10, 10, 10]
+            }
+        ]
     ]
 };
