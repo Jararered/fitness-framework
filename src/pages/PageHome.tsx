1fe0db1f7e60a3ffe1954a58143f29846b8e6fe1
Jararered <Jararered@icloud.com>
2025-01-07 07:58:05 -0500

    Add WorkoutPreview component to PageHome for displaying workout details

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,9 +1,16 @@
 import UserGreeting from '../components/v2/UserGreeting';
+import WorkoutPreview from '../components/v2/WorkoutPreview';
 
 const PageHome = () => {
     return (
         <div className="page-home">
+
             <UserGreeting />
+
+            <div className="card">
+                <WorkoutPreview />
+            </div>
+
         </div>
     );
 };
