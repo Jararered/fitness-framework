18fa0767442ce1136e028d7d0419b870ea203b9e
Jararered <Jararered@icloud.com>
2025-01-08 13:21:16 -0500

    Refactor CircuitPreview component for improved styling and structure; update class names for better clarity

--- src/components/v2/CircuitPreview.tsx
+++ src/components/v2/CircuitPreview.tsx
@@ -1,16 +1,18 @@
 import { Exercise, Sets } from "../../interfaces/Exercise";
 import { Circuit } from "../../interfaces/Workout";
 
-
 const CircuitPreview: React.FC<{ circuit: Circuit, circuitIndex: number, formatCommas: (sets: Sets) => string }> = ({ circuit, circuitIndex, formatCommas: FormatSets }) => (
-    <div className="circuit">
+    <div className="circuit-preview">
+
         <h3>Circuit {circuitIndex + 1}</h3>
+
         {circuit.map((exercise: Exercise, exerciseIndex: number) => (
-            <div key={exerciseIndex} className="exercise">
+            <div className="circuit-preview-exercise" key={exerciseIndex}>
                 <h4>{exercise.name}</h4>
                 <p>{exercise.plan?.sets ? FormatSets(exercise.plan.sets) : 'No reps available'}</p>
             </div>
         ))}
+
     </div>
 );
 
