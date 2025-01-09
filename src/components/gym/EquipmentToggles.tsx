22c8620a92188b3de9c2d47a858bd1fd4479bade
Jararered <Jararered@icloud.com>
2025-01-09 14:40:14 -0500

    Refactor EquipmentToggles component to simplify equipment state management and remove unused exercise state

--- src/components/gym/EquipmentToggles.tsx
+++ src/components/gym/EquipmentToggles.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useCallback } from "react";
 import { FaCheck } from "react-icons/fa";
 
 import { DefaultEquipment, Equipment, GetIconForEquipment } from "../../interfaces/Equipment"
-import { DefaultExercises, Exercise } from "../../interfaces/Exercise";
 
 const EquipmentToggles: React.FC = () => {
     const localEquipment = localStorage.getItem("equipment");
@@ -10,49 +9,28 @@ const EquipmentToggles: React.FC = () => {
         localEquipment ? JSON.parse(localEquipment) : DefaultEquipment
     );
 
-    const localExercises = localStorage.getItem("exercises");
-    const [exerciseState, setExerciseState] = useState<Exercise[]>(
-        localExercises ? JSON.parse(localExercises) : DefaultExercises
-    );
-
-    // Load equipment state from local storage or use default values
     useEffect(() => {
         localStorage.setItem("equipment", JSON.stringify(equipmentState));
     }, [equipmentState]);
 
-    // Save equipment state to local storage
-    const saveEquipmentLocal = useCallback((equipment: Equipment[]) => {
-        localStorage.setItem("equipment", JSON.stringify(equipment));
-    }, []);
-
-    // Handle toggling equipment
     const handleEquipmentToggle = useCallback((name: string) => {
-        setEquipmentState(prev => {
-            if (!prev) return prev;
-            const updatedEquipment = prev.map(equipment => {
-                if (equipment.name === name) {
-                    return equipment.config ? { ...equipment, config: { ...equipment.config, enabled: !equipment.config.enabled } } : equipment;
-                }
-                return equipment;
-            });
-            saveEquipmentLocal(updatedEquipment);
-            return updatedEquipment;
+        const newEquipmentState = equipmentState.map(equipment => {
+            if (equipment.name === name) {
+                equipment.config.enabled = !equipment.config.enabled;
+            }
+            return equipment;
         });
-    } , [saveEquipmentLocal]);
-
-    const handleResetEquipment = useCallback(() => {
-        setEquipmentState(DefaultEquipment);
-        saveEquipmentLocal(DefaultEquipment);
-    }, [saveEquipmentLocal]);
+        setEquipmentState(newEquipmentState);
+    }, [equipmentState]);
 
     return (
         <div className="equipment-toggles">
+            
             <h2>Equipment Toggles</h2>
 
             <div className="flexible-container">
                 {equipmentState?.map(equipment => (
-                    <div
-                        className={`small-card ${equipment.config.enabled ? "enabled" : ""}`}
+                    <div className={`small-card ${equipment.config.enabled ? "enabled" : ""}`}
                         key={equipment.name}
                         onClick={() => handleEquipmentToggle(equipment.name)}
                     >
@@ -64,7 +42,7 @@ const EquipmentToggles: React.FC = () => {
                 ))}
             </div>
 
-            <button className="bad-button" onClick={handleResetEquipment} >
+            <button className="bad-button" onClick={() => setEquipmentState(DefaultEquipment)}>
                 Reset Equipment
             </button>
 
