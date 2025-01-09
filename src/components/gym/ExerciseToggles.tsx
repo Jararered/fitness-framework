7d8ede3f72e93c6d14afd483be9805be35b1a3ce
Jararered <Jararered@icloud.com>
2025-01-09 14:44:43 -0500

    Refactor ExerciseToggles component to improve state management and simplify local storage handling

--- src/components/gym/ExerciseToggles.tsx
+++ src/components/gym/ExerciseToggles.tsx
@@ -1,57 +1,30 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FaCheck, FaDumbbell } from "react-icons/fa";
 
 import { Exercise, DefaultExercises } from "../../interfaces/Exercise";
-import { Equipment, DefaultEquipment } from "../../interfaces/Equipment";
 
 const ExerciseToggles = () => {
-    const [, setEquipmentState] = useState<Equipment[]>();
-    const [exerciseState, setExerciseState] = useState<Exercise[]>();
+    const localExercises = localStorage.getItem("exercises");
+    const [exerciseState, setExerciseState] = useState<Exercise[]>(
+        localExercises ? JSON.parse(localExercises) : DefaultExercises
+    );
 
-    // Load from local storage or use default values
     useEffect(() => {
-        const exercises = localStorage.getItem("exercises");
-        if (exercises) {
-            setExerciseState(JSON.parse(exercises));
-        } else {
-            setExerciseState(DefaultExercises);
-            saveExercisesLocal(DefaultExercises);
-        }
-
-        const equipment = localStorage.getItem("equipment");
-        if (equipment) {
-            setEquipmentState(JSON.parse(equipment));
-        }
-        else {
-            setEquipmentState(DefaultEquipment);
-        }
-
-    });
-
-    // Save to local storage
-    const saveExercisesLocal = (exercises: Exercise[]) => {
-        localStorage.setItem("exercises", JSON.stringify(exercises));
-    }
+        localStorage.setItem("exercises", JSON.stringify(exerciseState));
+    }, [exerciseState]);
 
     // Handle toggling exercises
-    const handleExerciseToggle = (name: string) => {
-        setExerciseState(prev => {
-            if (!prev) return prev;
-            const updatedExercises = prev.map(exercise => {
-                if (exercise.name === name) {
-                    return exercise.config ? { ...exercise, enabled: !exercise.config.enabled } : exercise;
+    const handleExerciseToggle = useCallback((name: string) => {
+        const newExerciseState = exerciseState.map(exercise => {
+            if (exercise.name === name) {
+                if (exercise.config) {
+                    exercise.config.enabled = !exercise.config.enabled;
                 }
-                return exercise;
-            });
-            saveExercisesLocal(updatedExercises);
-            return updatedExercises;
+            }
+            return exercise;
         });
-    }
-
-    const handleResetExercises = () => {
-        setExerciseState(DefaultExercises);
-        saveExercisesLocal(DefaultExercises);
-    }
+        setExerciseState(newExerciseState);
+    }, [exerciseState]);
 
     return (
         <div className="exercise-toggles">
@@ -59,25 +32,26 @@ const ExerciseToggles = () => {
             <h2>Exercise Toggles</h2>
 
             <div className="flexible-container">
-                {
-                    exerciseState && exerciseState.map(exercise => (
+                {exerciseState && exerciseState.map(exercise => (
                     <div
                         className={`small-card ${exercise.config && exercise.config.enabled ? "enabled" : ""}`}
                         onClick={() => handleExerciseToggle(exercise.name)}
                         key={exercise.name}
                     >
-                        <div className="equipment-icon">{<FaDumbbell size={48} />}</div>
+                        <div className="equipment-icon">{<FaDumbbell size={48}/>}</div>
                         <div className="small-card-name">{exercise.name}</div>
-                        <div className="checkmark-icon">{exercise.config && exercise.config.enabled && <FaCheck size={24} />} </div>
+                        <div className="checkmark-icon">{exercise.config && exercise.config.enabled && <FaCheck size={24}/>} </div>
                     </div>
                 ))}
             </div>
 
-            <button className="bad-button" onClick={handleResetExercises}>
+            <button
+                className="bad-button"
+                onClick={() => setExerciseState(DefaultExercises)}>
                 Reset Exercises
             </button>
 
-        </div>
+        </div >
     );
 }
 
