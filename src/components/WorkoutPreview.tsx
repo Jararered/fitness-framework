2f54b846263e9605479976b5a5870ea3efcce94a
Jararered <Jararered@icloud.com>
2025-01-14 17:28:15 -0500

    Enhance WorkoutPreview component to display a message when no circuits are present and improve circuit rendering

--- src/components/WorkoutPreview.tsx
+++ src/components/WorkoutPreview.tsx
@@ -1,7 +1,5 @@
-import { Workout, Circuit } from "../interfaces/Workout";
+import { Workout } from "../interfaces/Workout";
 import { LegsExampleWorkout } from "../interfaces/Examples";
-import { FormatSets } from "../utils/Formatting";
-import CircuitPreview from "./CircuitPreview";
 import LocalStorage, { Keys } from "../interfaces/Storage";
 
 const WorkoutPreview: React.FC = () => {
@@ -12,10 +10,24 @@ const WorkoutPreview: React.FC = () => {
 
             <h2>Workout Preview</h2>
 
-            {workout.circuits.map((circuit: Circuit, circuitIndex: number) => (
-                <CircuitPreview key={circuitIndex} circuit={circuit} circuitIndex={circuitIndex} formatCommas={FormatSets} />
-            ))}
+            {workout.circuits.length === 0 ? (
+                <p>No circuits in workout, add some in the editor.</p>
+            ) : (
+                workout.circuits.map((circuit, circuitIndex) => (
+                    <div className={"circuit-" + circuitIndex} key={circuitIndex}>
 
+                        <h3>Circuit {circuitIndex + 1}</h3>
+
+                        {circuit.map((exercise, exerciseIndex) => (
+                            <div className="flex" key={exerciseIndex}>
+                                <b>{exercise.name}</b>
+                                <p>{exercise.plan?.sets.join(', ')} sets</p>
+                            </div>
+                        ))}
+
+                    </div>
+                ))
+            )}
         </div>
     );
 };
