0b9a10420dcae8cd54d802f7b4abdae62d4cb9a4
Jararered <Jararered@icloud.com>
2025-01-14 17:53:50 -0500

    Add additional exercise names to ExerciseNames array for expanded workout options

--- src/interfaces/Exercise.tsx
+++ src/interfaces/Exercise.tsx
@@ -52,6 +52,60 @@ export type ExerciseName =
     "Smith Machine Shrugs" |
     "Smith Machine Squat";
 
+export const ExerciseNames: ExerciseName[] = [
+    "Barbell Bench Press",
+    "Barbell Bent-Over Row",
+    "Barbell Curl",
+    "Barbell Deadlift",
+    "Barbell Decline Bench Press",
+    "Barbell Front Squat",
+    "Barbell Incline Bench Press",
+    "Barbell Lunge",
+    "Barbell Overhead Press",
+    "Barbell Romanian Deadlift",
+    "Barbell Squat",
+    "Barbell Sumo Deadlift",
+    "Dumbbell Bent-Over Row",
+    "Dumbbell Bicep Curl",
+    "Dumbbell Front Raise",
+    "Dumbbell Hammer Curl",
+    "Dumbbell Lateral Raise",
+    "Dumbbell Lunges",
+    "Dumbbell Shoulder Press",
+    "Dumbbell Shrugs",
+    "Dumbbell Single-Arm Row",
+    "Dumbbell Skull Crushers",
+    "Dumbbell Tricep Extension",
+    "Dumbbell Upright Row",
+    "EZ Bar Close-Grip Curl",
+    "EZ Bar Curl",
+    "EZ Bar Skull Crusher",
+    "EZ Bar Wide-Grip Curl",
+    "Lat Pulldown",
+    "Lat Pulldown Behind-the-Head",
+    "Lat Pulldown Narrow-Grip",
+    "Lat Pulldown Reverse-Grip",
+    "Lat Pulldown Single-Arm",
+    "Lat Pulldown Wide-Grip",
+    "Leg Curl",
+    "Leg Curl + Isometric Hold",
+    "Leg Extension",
+    "Leg Extension + Isometric Hold",
+    "Leg Press",
+    "Machine Chest Press",
+    "Machine Incline Chest Press",
+    "Machine Shoulder Press",
+    "Smith Machine Bench Press",
+    "Smith Machine Bent-Over Row",
+    "Smith Machine Close-Grip Bench Press",
+    "Smith Machine Incline Bench Press",
+    "Smith Machine Lunges",
+    "Smith Machine Shoulder Press",
+    "Smith Machine Shrugs",
+    "Smith Machine Squat"
+];
+
+
 // Exercise Type
 export type ExerciseCategory =
     "Legs" |
