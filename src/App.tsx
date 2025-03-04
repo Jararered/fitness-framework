43b19e72913bd84c6e7672e7e10ec68130848fdc
Jararered <Jararered@icloud.com>
2025-03-04 13:40:57 -0500

    Remove unused components and styles; delete Formatting utility, card styles, and various page components for improved code cleanliness and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,11 @@
-import React, { useState } from "react";
-
-import { DockBar } from "./components/Navigation";
-
-// Pages
-import PageHome from "./pages/PageHome";
-import PageWorkoutEditor from "./pages/PageWorkoutEditor";
-import PageGym from "./pages/PageGym";
-import PageProfile from "./pages/PageProfile";
-import PageSettings from "./pages/PageSettings";
-
-// Styles
-import "./App.css";
-import "./styles/button.css";
-import "./styles/card.css";
-
-
-export type SetPageProps = {
-    setPage: React.Dispatch<React.SetStateAction<string>>;
-};
+import React from "react";
 
 const App: React.FC = () => {
-    const [page, setPage] = useState<string>("home");
-
-    const renderMainContent = () => {
-        switch (page) {
-
-            case "gym":
-                return <PageGym />;
-            case "home":
-                return <PageHome setPage={setPage} />;
-            case "workout":
-                return <PageWorkoutEditor />;
-            case "profile":
-                return <PageProfile />;
-            case "settings":
-                return <PageSettings />;
-
-            default:
-                return <PageHome setPage={setPage} />;
-        }
-    };
-
     return (
         <div className="App">
             <div className="main-content">
-                {renderMainContent()}
+                Hello world!
             </div>
-
-            <DockBar setContent={setPage} />
         </div>
     );
 };
