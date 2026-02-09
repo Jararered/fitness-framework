c21e13d136d3c887b84c40abad2f67ea238853fb
Jararered <Jararered@icloud.com>
2026-02-09 06:10:19 -0500

    Remove ApiTester component and associated styles from HomePage for improved clarity and maintainability

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,15 +3,12 @@ import React from "react";
 import { Card } from "../features/layout/components/Card.tsx";
 import WorkoutPreview from "../features/workouts/components/WorkoutPreview.tsx";
 
-import ApiTester from "../app/ApiTester.tsx";
+
 
 const HomePage: React.FC = () => {
   return (
     <div className="home-page page-container">
       <h1>Home</h1>
-
-      <ApiTester />
-
       <Card
         title="Workout Preview"
         description="Below will show a preview of your workout once you have loaded a workout."
